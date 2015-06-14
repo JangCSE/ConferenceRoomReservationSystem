b8dccb184acd46b398840c13dc4bacd9f1b47ab6
hotaek95 <hoteak95@naver.com>
2015-06-14 20:01:00 +0900

    add print text file function

--- ConferenceRoomReservationSystem/src/enterprise/listRoom/listRoomController.java
+++ ConferenceRoomReservationSystem/src/enterprise/listRoom/listRoomController.java
@@ -2,6 +2,8 @@ package enterprise.listRoom;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import master.ClientMasterController;
@@ -12,6 +14,8 @@ public class listRoomController implements ActionListener {
 	private listRoomModel lrm;
 	private listRoomView lrv;
 	private TransmissionData data;
+	private FileWriter fw;
+	private File dir;
 
 	public listRoomController(listRoomModel lm, listRoomView lv) {
 		lrm = lm;
@@ -37,6 +41,26 @@ public class listRoomController implements ActionListener {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			return;
+		} else if (arg0.getActionCommand().equals("파일에 저장")) {
+			fw = null;
+			dir = new File("C:\\ConferenceRoomReservationSystem");
+			if (!dir.exists()) {
+				dir.mkdir();
+			}
+			try {
+				fw = new FileWriter("C:\\ConferenceRoomReservationSystem\\"
+						+ shm.getSelectedRoom().getName() + ".txt");
+				fw.write(shm.getSelectedRoom().getName());
+				fw.write(shm.getSelectedRoom().getCity());
+				fw.write(shm.getSelectedRoom().getDetailLocation());
+				fw.write(Integer.toString(shm.getSelectedRoom().getMaxNumber()));
+				fw.write(Integer.toString(shm.getSelectedRoom().getCost()));
+				fw.write(shm.getSelectedRoom().getDetail());
+				fw.close();
+			} catch (Exception e) {
+				System.out.println("error : " + e);
+			}		
 			return;
 		}
 
