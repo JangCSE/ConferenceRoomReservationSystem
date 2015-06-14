147fd7255d5b1e88b8030659a9eb26720eca98fd
HyoWon Jang <jang.hw73@gmail.com>
2015-06-14 16:16:01 +0900

    Revert "classNotFound Solve"
    This reverts commit 21045a96e7966676007986dff3c6ceb65ead4c38.

--- ConferenceRoomReservationSystem/src/enterprise/registerRoom/RegisterRoomController.java
+++ ConferenceRoomReservationSystem/src/enterprise/registerRoom/RegisterRoomController.java
@@ -2,10 +2,10 @@ package enterprise.registerRoom;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import java.io.IOException;
 
 import master.ClientMasterController;
+import server.room.Room;
 import transmission.TransmissionData;
 
 public class RegisterRoomController implements ActionListener {
@@ -37,7 +37,7 @@ public class RegisterRoomController implements ActionListener {
 
 		if (arg0.getActionCommand().equals("되돌아가기")) {
 			data = new TransmissionData();
-			data.setFlags(2); // 무슨 값 해야되는지 모름~
+			data.setFlags(30); // 무슨 값 해야되는지 모름~
 			try {
 				ClientMasterController.getClient().sendToServer(data);
 			} catch (IOException e) {
@@ -49,13 +49,48 @@ public class RegisterRoomController implements ActionListener {
 
 		rrm.setMessage(rrv.getMsgStr());
 		rrm.setName(rrv.getNameStr());
-		rrm.setMaxNumber(Integer.parseInt(rrv.getMaxNumStr()));
-		rrm.setCost(Integer.parseInt(rrv.getCost()));
+		
+		try {
+			rrm.setMaxNumber(Integer.parseInt(rrv.getMaxNumStr()));
+		} catch (NumberFormatException e) {
+			rrm.setMessage("수용인원은 숫자로 입력해 주십시오.");
+			return;
+		}
+		
+		try {
+			rrm.setCost(Integer.parseInt(rrv.getCost()));
+		} catch (NumberFormatException e) {
+			rrm.setMessage("대여비용은 숫자로 입력해 주십시오.");
+			return;
+		}
+		
 		rrm.setCity(rrv.getCityStr());
 		rrm.setDetailLocation(rrv.getDetailLoc());
 		rrm.setDetail(rrv.getDetail());
 
-		// 다른 처리 필요한가?
+		if(rrm.validCheck() == 1) {
+			rrm.setMessage("회의실 위치가 잘못되었습니다.");
+		} else if(rrm.validCheck() == 2) {
+			rrm.setMessage("수용가능 인원은 최소 두 명에서 최대 만 명입니다.");
+		} else if(rrm.validCheck() == 3) {
+			rrm.setMessage("대여비용은 최소 만 원으로 천 원 단위로 증가시킬 수 있습니다.");
+		} else if(rrm.validCheck() == 4) {
+			rrm.setMessage("회의실 이름은 한 자 이상 스무 자 이하로 입력하셔야 합니다.");
+		} else if(rrm.validCheck() == 5) {
+			rrm.setMessage("부가정보는 최대 500자까지 입니다.");
+		} else {
+			data = new TransmissionData();
+			data.setFlags(20);
+			data.setRoom(new Room(rrm.getName(), rrm.getCity(), rrm.getDetailLocation(), rrm.getMaxNumber(), rrm.getCost(), rrm.getDetail()));
+			try {
+				ClientMasterController.getClient().sendToServer(data);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return;
+			
+		}
 
 	}
 
